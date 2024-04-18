443e027f543a31ebd52c2ed5d1fa868d57b8b805
mayankjaiswal27 <mayankjaiswal0104@gmail.com>
2024-04-18 16:27:47 +0530

    Initial commit

---
+++ backend/database/dbConnection.js
@@ -0,0 +1,15 @@
+import { configDotenv } from "dotenv";
+import mongoose from "mongoose";
+
+export const dbConnection = () => {
+  mongoose
+    .connect(process.env.MONGO_URI, {
+      dbName: "bloglane",
+    })
+    .then(() => {
+      console.log("Connected to database!");
+    })
+    .catch((err) => {
+      console.log(`Some error occured while connecting to database: ${err}`);
+    });
+};
